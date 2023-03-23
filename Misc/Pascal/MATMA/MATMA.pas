5cc78b39daab2d5acc412bedc0a5d65906f3f3ff
sandvichofTF2 <long05202@gmail.com>
2023-03-23 21:45:29 +0700

    pas

--- Misc/Pascal/MATMA/MATMA.pas
+++ Misc/Pascal/MATMA/MATMA.pas
@@ -1,39 +1,39 @@
-const fi = 'MATMA.inp';
-	  fo = 'MATMA.out';
+// const fi = 'MATMA.inp';
+// 	  fo = 'MATMA.out';
 
-var
-	n,size,i: Integer;
-	s: AnsiString;
-	a: array['A'..'Z'] of integer;
-	k : Char;
-begin
-	assign(input,fi); reset(input);
-	assign(output,fo); rewrite(output);
+// var
+// 	n,size,i: Integer;
+// 	s: AnsiString;
+// 	a: array['A'..'Z'] of integer;
+// 	k : Char;
+// begin
+// 	assign(input,fi); reset(input);
+// 	assign(output,fo); rewrite(output);
 	
-	size := 0;
-	// k:= 'A';
-	while (not(eof(input))) do 
-	begin
-		readln(input,s);
-		for i:= 0 to length(s) do
-		begin
-			k := s[i];
-			if ((k >= 'A') and (k <= 'Z')) then
-			begin
+// 	size := 0;
+// 	// k:= 'A';
+// 	while (not(eof(input))) do 
+// 	begin
+// 		readln(input,s);
+// 		for i:= 0 to length(s) do
+// 		begin
+// 			k := s[i];
+// 			if ((k >= 'A') and (k <= 'Z')) then
+// 			begin
 				
-				a[k] := a[k] +1;
-			end;	
-		end;
-	end;
-	for k := 'A' to 'Z' do 
-	begin
-		if ((a[k] < 9) and (a[k] > 0)) then write(a[k]);
-	end;
+// 				a[k] := a[k] +1;
+// 			end;	
+// 		end;
+// 	end;
+// 	for k := 'A' to 'Z' do 
+// 	begin
+// 		if ((a[k] < 9) and (a[k] > 0)) then write(a[k]);
+// 	end;
 	
 
 	
 
 
 
-	close(input); close(output);
-end.
+// 	close(input); close(output);
+// end.
